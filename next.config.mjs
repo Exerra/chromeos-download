5d913d828086ed06a7d612557fb53f7c9880ab83
Exerra <exerra@exerra.xyz>
2023-03-12 22:42:28 +0200

    changed footer img to <Image />

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    fontLoaders: [
-      {
-        loader: "@next/font/google",
-        options: { subsets: ["latin"] },
-      },
-    ],
-  },
+	reactStrictMode: true,
+	experimental: {
+		fontLoaders: [
+			{
+				loader: "@next/font/google",
+				options: { subsets: [ "latin" ] },
+			},
+		],
+	},
+	images: {
+		domains: [
+			"cdn.exerra.xyz"
+		]
+	}
 }
 
 export default nextConfig
